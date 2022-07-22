13f82df1da5d1e0d4173e00a10f2ea83c740870f
Kamuran ÖZTÜRK <105446944+kamuranoztrk@users.noreply.github.com>
2022-07-22 17:03:33 +0300

    Create odev1.sql

---
+++ odev1.sql
@@ -0,0 +1,16 @@
+1-
+SELECT title, description FROM film;
+
+SELECT * FROM film 
+WHERE (length>60 AND length<75);
+
+SELECT * FROM film 
+Where rental_rate = 0.99 AND (replacement_cost = 12.99 OR replacement_cost = 28.99);
+
+SELECT first_name, last_name FROM customer 
+WHERE first_name = 'MARY';
+
+SELECT * FROM film
+WHERE length<50 AND (rental_rate != 2.99 AND rental_rate != 4.99); 
+
+
